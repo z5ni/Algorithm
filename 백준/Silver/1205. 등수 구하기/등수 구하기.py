75e97c7d312b04512af8151742b7bea43e245f6c
zion <0o0w0d@gmail.com>
2024-11-23 21:55:23 +0900

    [Silver IV] Title: 등수 구하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -1,11 +1,10 @@
-N, new_point, P = map(int, input().split())
+N, point, P = map(int, input().split())
 rank = list(map(int, input().split())) if N > 0 else []
 
-new_rank = rank[:P]
-new_rank.append(new_point)
-new_rank.sort(reverse=True)
+rank.append(point)
+rank.sort(reverse=True)
 
-if new_rank.index(new_point) + new_rank.count(new_point) - 1 < P:
-	print(new_rank.index(new_point) + 1)
+if rank.index(point) + rank.count(point) - 1 < P:
+	print(rank.index(point) + 1)
 else:
 	print(-1)
