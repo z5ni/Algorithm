8835170b4f703a0269adc90769bc4420538a5dc7
zion <0o0w0d@gmail.com>
2025-02-10 22:51:48 +0900

    [Silver IV] Title: 로프, Time: 2592 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,15 +1,9 @@
 N = int(input())
-ropes = []
-
-for _ in range(N):
-    rope = int(input())
-    ropes.append(rope)
-
-ropes.sort(reverse=True)
-max_weight = 0
+ropes = sorted([int(input()) for _ in range(N)], reverse=True)
+result = -1
+# 현재 로프 * 현재까지의 로프 개수
 
 for i in range(N):
-    weight = ropes[i] * (i + 1)
-    max_weight = max(max_weight, weight)
-
-print(max_weight)
+	weight = ropes[i] * (i+1)
+	result = max(result, weight)
+print(result)
