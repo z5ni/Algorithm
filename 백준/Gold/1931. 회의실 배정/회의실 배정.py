ca8a878c68047b3e1313a976f3f9941f6dd3ac2a
zion <0o0w0d@gmail.com>
2025-02-23 00:25:44 +0900

    [Gold V] Title: 회의실 배정, Time: 2756 ms, Memory: 55000 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,11 +1,12 @@
 N = int(input())
 lst = [tuple(map(int, input().split())) for _ in range(N)]
 lst = sorted(lst, key=lambda x: (x[1], x[0]))
-result = []
-result.append(lst[0])
+cnt = 1
+end_time = lst[0][1]
 
 for start, end in lst[1:]:
-	if start >= result[-1][1]:
-		result.append((start, end))
+	if start >= end_time:
+		cnt += 1
+		end_time = end
 
-print(len(result))
+print(cnt)
