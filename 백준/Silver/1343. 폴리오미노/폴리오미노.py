950d0a1554405de080304af824820453419aaccd
zion <0o0w0d@gmail.com>
2025-02-25 23:55:27 +0900

    [Silver V] Title: 폴리오미노, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,18 +1,8 @@
 S = input()
-S = list(S.split('.'))
-result = []
-check = True
+S = S.replace("XXXX", "AAAA")
+S = S.replace("XX", "BB")
 
-for s in S:
-	a = len(s) // 4
-	remi = len(s) % 4
-	b = remi // 2
-	result.append(4 * a * "A" + 2 * b * "B")
-	if remi % 2 != 0:
-		check = False
-
-if check:
-	print('.'.join(result))
-else:
+if "X" in S:
 	print(-1)
-
+else:
+	print(S)
