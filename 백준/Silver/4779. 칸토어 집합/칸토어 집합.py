d645cdee7502431a20501eca1c13651cd9df059d
zion <0o0w0d@gmail.com>
2025-07-14 12:30:39 +0900

    [Silver III] Title: 칸토어 집합, Time: 36 ms, Memory: 33796 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,13 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
 def canto(n):
 	if n == 0:
-		return "-"
-	else:
-		return canto(n-1) + ' ' * 3 ** (n-1) + canto(n-1)
-
+		return '-'
+	return canto(n - 1) + ' ' * 3 ** (n - 1) + canto(n - 1) 
 
 while True:
 	try:
-		N = int(input())
-		print(canto(N))
-	except Exception as e:
+		n = int(input().rstrip())
+		print(canto(n))
+	
+	except:
 		break
