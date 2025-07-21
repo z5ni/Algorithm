92b943ecd3656c2fc994dcc23438b2e386c91e27
zion <0o0w0d@gmail.com>
2025-07-21 16:19:13 +0900

    [Silver II] Title: 부분수열의 합, Time: 576 ms, Memory: 82416 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,18 +1,15 @@
+import sys
 from itertools import combinations
 
-N, S = map(int, input().split())
-lst = list(map(int, input().split()))
+input = sys.stdin.readline
+n, s = map(int, input().rstrip().split())
+lst = list(map(int, input().rstrip().split()))
 cnt = 0
 
-# 1~N개까지의 수열을 만들 수 있음
-# 가능한 리스트 조합 다 만들어놓고 sum이 S와 같은 경우의 개수를 구하자?
-
-all_list = []
-for i in range(1, len(lst) + 1):
-	all_list.extend(list(combinations(lst, i)))
-
-for ls in all_list:
-	if sum(ls) == S:
-		cnt += 1
+for i in range(1, n+1):
+	P = list(combinations(lst, i))
+	for p in P:
+		if sum(p) == s:
+			cnt += 1
 
 print(cnt)
